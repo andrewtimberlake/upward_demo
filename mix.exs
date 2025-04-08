2e8f6b099c9ca127b167d1d49ce5b1fc85fc2e1e
Andrew Timberlake <andrew@andrewtimberlake.com>
2025-04-08 17:03:19 +0200

    Release 0.0.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule UpwardDemo.MixProject do
   def project do
     [
       app: :upward_demo,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
